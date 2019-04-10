fb3d85f21ecd505362b70b0f8429ce2022f23fee
revdancatt <github@revdancatt.com>
2019-04-10 16:30:24 +0100

    Our first mutation

--- src/routes/administration/instances/index.js
+++ src/routes/administration/instances/index.js
@@ -1,15 +1,17 @@
-const Queries = require('../../../classes/queries')
+// const Queries = require('../../../classes/queries')
+const Mutations = require('../../../classes/mutations')
 const GraphQL = require('../../../classes/graphQL')
 
 exports.index = async (req, res) => {
   if (!req.user || !req.user.roles || !req.user.roles.isAdmin) return res.render('main/redirect', req.templateValues)
 
-  const queries = new Queries()
+  // const queries = new Queries()
+  const mutations = new Mutations()
   const graphQL = new GraphQL()
 
-  let query = queries.get('hello', '')
+  let mutation = mutations.get('createInstance', `(title:"fnord")`)
   const payload = {
-    query
+    query: mutation
   }
   const records = await graphQL.fetch(payload, req.user.apitoken)
   console.log(records)
