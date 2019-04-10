e68716478e1e26c035581c290657a8802c27fd9b
revdancatt <github@revdancatt.com>
2019-04-10 14:21:39 +0100

    Run a query

---
+++ src/classes/graphQL/index.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-useless-constructor */
+const request = require('request-promise')
+const Config = require('../config')
+
+/** Class allowing us to connect to a GraphQL server. */
+class GraphQL {
+  /**
+   * Once again, wecause we are dealing with async stuff we have an empty constructor
+   * so we can have a GraphQL object that we can then drop a handy method onto. We could
+   * just turn this into a module, but I suspect we'll need this as a class in the
+   * near future. So rather than refactor later I'm putting this here.
+   */
+  constructor () {}
+
+  /**
+   *
+   * @param {json} payload The query and any other options to be sent to the GraphQL server
+   * @returns {json|Array} The results from GraphQL as a json objects, or an array containing the error if the query failed
+   */
+  async fetch (payload, token) {
+    //  Grab the graphQL host
+    const configObj = new Config()
+    const graphQL = configObj.get('graphQL')
+    if (!graphQL) return []
+
+    const headers = {
+      'content-type': 'application/json',
+      Authorization: `bearer ${token}`
+    }
+    return request({
+      url: `${graphQL}/graphql`,
+      method: 'POST',
+      headers,
+      json: payload
+    })
+      .then(response => {
+        return response
+      })
+      .catch(error => {
+        console.log(error)
+        return []
+      })
+  }
+}
+module.exports = GraphQL
--- src/routes/administration/instances/index.js
+++ src/routes/administration/instances/index.js
@@ -1,7 +1,17 @@
-const Instances = require('../../../classes/instances')
+const Queries = require('../../../classes/queries')
+const GraphQL = require('../../../classes/graphQL')
 
 exports.index = async (req, res) => {
   if (!req.user || !req.user.roles || !req.user.roles.isAdmin) return res.render('main/redirect', req.templateValues)
-  req.templateValues.instances = await new Instances().get()
+
+  const queries = new Queries()
+  const graphQL = new GraphQL()
+
+  let query = queries.get('hello', '')
+  const payload = {
+    query
+  }
+  const records = await graphQL.fetch(payload, req.user.apitoken)
+  console.log(records)
   return res.render('administration/instances/index', req.templateValues)
 }
